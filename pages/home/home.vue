47e0f124e00ee126627252840697242c637f3c19
GTY <a15302948444@gmail.com>
2022-03-01 21:13:44 +0800

    完成了tabBar

---
+++ pages/home/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>
+    home11
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style>
+
+</style>
