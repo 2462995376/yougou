043754a463ab3940fa1e2b4695d7c2830836f672
GTY <a15302948444@gmail.com>
2022-03-07 10:15:33 +0800

    完成了商品列表页

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ uni.$showMsg=function(title="数据加载失败",duration=1500){
   return uni.showToast({
     title,
     duration,
-    icon:none
+    icon:'none'
   })
 }
 
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,19 +1,73 @@
 <template>
   <view>
-    list
+    <view class="goods-list">
+      <view v-for="(item, i) in goodsList" :key="i" @click="gotoDetail(item)">
+        <my-goods :goods="item"></my-goods>
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
   export default {
+    onLoad(options) {
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      this.getGoodsList()
+    },
+    onPullDownRefresh() {
+      // 1. 重置关键数据
+      this.queryObj.pagenum = 1
+      this.total = 0
+      this.isloading = false
+      this.goodsList = []
+
+      // 2. 重新发起请求
+      this.getGoodsList(() => uni.stopPullDownRefresh())
+    },
+    onReachBottom() {
+      if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
+      if (this.isLoading) return
+      this.queryObj.pagenum++
+      this.getGoodsList()
+    },
     data() {
       return {
-        
+        queryObj: {
+          // 查询关键词
+          query: '',
+          // 商品分类Id
+          cid: '',
+          // 页码值
+          pagenum: 1,
+          // 每页显示多少条数据
+          pagesize: 10
+        },
+        goodsList: [],
+        total: 0,
+        isLoading: false
       };
+    },
+    methods: {
+      async getGoodsList(cb) {
+        this.isLoading = true
+        const {
+          data: res
+        } = await uni.$http.get('/goods/search', this.queryObj)
+        if (res.meta.status !== 200) return uni.$showMsg()
+        this.isLoading = false
+        cb && cb();
+        this.total = res.message.total
+        this.goodsList = [...res.message.goods, ...this.goodsList]
+      },
+      gotoDetail(item) {
+        uni.navigateTo({
+          url: '../goods_detail/goods_detail?goods_id=' +item.goods_id
+        })
+      }
     }
   }
 </script>
 
 <style lang="scss">
-
 </style>
